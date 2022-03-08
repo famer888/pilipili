840ebbe9f9cc129fcf3f1159c0b2e666799da606
Tom <toms64816@gmail.com>
2022-03-08 11:20:29 +0700

    1

---
+++ lib/pages/detail/comicsDetail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Comicsdetail extends StatefulWidget {
+  const Comicsdetail({Key key}) : super(key: key);
+
+  @override
+  _ComicsdetailState createState() => _ComicsdetailState();
+}
+
+class _ComicsdetailState extends State<Comicsdetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
