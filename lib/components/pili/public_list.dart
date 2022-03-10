0a07dd7bbc903615a13da77a1c2cb5af903f7c63
li <lilei961002@gmail.com>
2022-03-10 17:45:22 +0700

    save

--- lib/components/pili/public_list.dart
+++ lib/components/pili/public_list.dart
@@ -15,12 +15,13 @@ import 'package:waterfall_flow/waterfall_flow.dart';
 
 class PublicList extends StatefulWidget {
   @required
-  final bool isShow;//是否展示
-  final String api;//接口地址
-  final Map data;//传递参数
+  final bool isShow; //是否展示
+  final String api; //接口地址
+  final Map data; //传递参数
   final int limit;
-  final bool isFlow;//是否瀑布流
+  final bool isFlow; //是否瀑布流
   final String cartType; //  "h" 横向card  "v"竖向card
+  final int contentType; //参照 cardMixin.dart 文件
   PublicList(
       {Key key,
       this.isShow,
@@ -28,7 +29,8 @@ class PublicList extends StatefulWidget {
       this.data,
       this.limit = 20,
       this.isFlow = true,
-      this.cartType = 'h'})
+      this.cartType = 'h',
+      this.contentType})
       : super(key: key);
 
   @override
@@ -108,86 +110,91 @@ class _PublicListState extends State<PublicList> {
 
   @override
   Widget build(BuildContext context) {
-    return loading? 
-    PageStatus.loading(true)
-    :searchData.isEmpty?
-    PageStatus.noData()
-    :PullRefreshList(
-      onLoading: () {
-        if (isAll) {
-          CommonUtils.showText('已为您加载完所有数据～');
-          return;
-        }
-        reqData['page']++;
-        getSearchResult();
-      },
-      onRefresh: () {
-        reqData['page'] = 1;
-        isAll = false;
-        getSearchResult();
-      },
-      child: widget.isFlow
-          ? WaterfallFlow.builder(
-              primary: false,
-              padding: EdgeInsets.only(
-                  top: DefaultStyle.pagePadding,
-                  bottom: MediaQuery.of(context).padding.bottom +
-                      ScreenUtil().bottomBarHeight,
-                  left: DefaultStyle.pagePadding,
-                  right: DefaultStyle.pagePadding),
-              itemCount: searchData.length,
-              gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: ScreenUtil().setWidth(10),
-                  crossAxisSpacing: ScreenUtil().setWidth(10)),
-              itemBuilder: (BuildContext context, int index) {
-                return searchData[index]['mv_type'] == 1
-                    ? Hcard(
-                        width: ScreenUtil().setWidth(175),
-                        tagIconType: searchData[index]['isfree'],
-                        thumbUrl: CommonUtils.getThumb(searchData[index]),
-                        contentType: 1,
-                        cardData: searchData[index],
-                        showField: 'title')
-                    : Vcard(
-                        width: ScreenUtil().setWidth(175),
-                        isSearch: true,
-                        tagIconType: searchData[index]['isfree'],
-                        thumbUrl: CommonUtils.getThumb(searchData[index]),
-                        contentType: 7,
-                        cardData: searchData[index],
-                        showField: 'title');
-                ;
-              })
-          : GridView.builder(
-              cacheExtent: ScreenUtil().screenHeight * 5,
-              padding: EdgeInsets.symmetric(
-                  horizontal: DefaultStyle.pagePadding,
-                  vertical: ScreenUtil().setWidth(20)),
-              itemCount: searchData.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: ScreenUtil().setWidth(7),
-                crossAxisSpacing: ScreenUtil().setWidth(7),
-                childAspectRatio: 1.11,
-              ),
-              itemBuilder: (context, index) {
-                return widget.cartType == 'h'
-                    ? Hcard(
-                        width: ScreenUtil().setWidth(175),
-                        tagIconType: searchData[index]['isfree'],
-                        thumbUrl: CommonUtils.getThumb(searchData[index]),
-                        contentType: 1,
-                        cardData: searchData[index],
-                        showField: 'title')
-                    : Vcard(
-                        width: ScreenUtil().setWidth(175),
-                        tagIconType: searchData[index]['isfree'],
-                        thumbUrl: CommonUtils.getThumb(searchData[index]),
-                        contentType: 1,
-                        cardData: searchData[index],
-                        showField: 'title');
-              }),
-    );
+    return loading
+        ? PageStatus.loading(true)
+        : searchData.isEmpty
+            ? PageStatus.noData()
+            : PullRefreshList(
+                onLoading: () {
+                  if (isAll) {
+                    CommonUtils.showText('已为您加载完所有数据～');
+                    return;
+                  }
+                  reqData['page']++;
+                  getSearchResult();
+                },
+                onRefresh: () {
+                  reqData['page'] = 1;
+                  isAll = false;
+                  getSearchResult();
+                },
+                child: widget.isFlow
+                    ? WaterfallFlow.builder(
+                        primary: false,
+                        padding: EdgeInsets.only(
+                            top: DefaultStyle.pagePadding,
+                            bottom: MediaQuery.of(context).padding.bottom +
+                                ScreenUtil().bottomBarHeight,
+                            left: DefaultStyle.pagePadding,
+                            right: DefaultStyle.pagePadding),
+                        itemCount: searchData.length,
+                        gridDelegate:
+                            SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: ScreenUtil().setWidth(10),
+                                crossAxisSpacing: ScreenUtil().setWidth(10)),
+                        itemBuilder: (BuildContext context, int index) {
+                          return searchData[index]['mv_type'] == 1
+                              ? Hcard(
+                                  width: ScreenUtil().setWidth(175),
+                                  tagIconType: searchData[index]['isfree'],
+                                  thumbUrl:
+                                      CommonUtils.getThumb(searchData[index]),
+                                  contentType: widget.contentType ?? 1,
+                                  cardData: searchData[index],
+                                  showField: 'title')
+                              : Vcard(
+                                  width: ScreenUtil().setWidth(175),
+                                  isSearch: true,
+                                  tagIconType: searchData[index]['isfree'],
+                                  thumbUrl:
+                                      CommonUtils.getThumb(searchData[index]),
+                                  contentType: widget.contentType ?? 7,
+                                  cardData: searchData[index],
+                                  showField: 'title');
+                          ;
+                        })
+                    : GridView.builder(
+                        cacheExtent: ScreenUtil().screenHeight * 5,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: DefaultStyle.pagePadding,
+                            vertical: ScreenUtil().setWidth(20)),
+                        itemCount: searchData.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: ScreenUtil().setWidth(7),
+                          crossAxisSpacing: ScreenUtil().setWidth(7),
+                          childAspectRatio: 1.11,
+                        ),
+                        itemBuilder: (context, index) {
+                          return widget.cartType == 'h'
+                              ? Hcard(
+                                  width: ScreenUtil().setWidth(175),
+                                  tagIconType: searchData[index]['isfree'],
+                                  thumbUrl:
+                                      CommonUtils.getThumb(searchData[index]),
+                                  contentType: widget.contentType ?? 1,
+                                  cardData: searchData[index],
+                                  showField: 'title')
+                              : Vcard(
+                                  width: ScreenUtil().setWidth(175),
+                                  tagIconType: searchData[index]['isfree'],
+                                  thumbUrl:
+                                      CommonUtils.getThumb(searchData[index]),
+                                  contentType: widget.contentType ?? 1,
+                                  cardData: searchData[index],
+                                  showField: 'title');
+                        }),
+              );
   }
 }
