130d71e83c0f4e97cfcdafd3e9808577b9d9a20e
li <lilei961002@gmail.com>
2022-03-25 14:20:17 +0700

    save

--- lib/components/widget/h_scroll_widget.dart
+++ lib/components/widget/h_scroll_widget.dart
@@ -55,7 +55,7 @@ class _HscrollWidgetState extends State<HscrollWidget> {
                 )
               : Container(),
           Container(
-            height: ScreenUtil().setWidth(170),
+            height: ScreenUtil().setWidth(175),
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               padding: EdgeInsets.only(left: DefaultStyle.pagePadding),
