5d9d53d69cebe1497ecd4794804844804c2532b7
小語 <wumeiuo@gmail.com>
2023-01-07 15:48:30 +0800

    save

--- lib/components/common/images.dart
+++ lib/components/common/images.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pilipili/utils/networkImage.dart';
 
 Widget getImage(String url,
