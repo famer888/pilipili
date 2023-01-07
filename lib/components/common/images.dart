602e57cf9676c634df894a19d60a0c8397da73e2
小語 <wumeiuo@gmail.com>
2023-01-07 17:28:17 +0800

    save

--- lib/components/common/images.dart
+++ lib/components/common/images.dart
@@ -3,18 +3,18 @@ import 'package:pilipili/utils/networkImage.dart';
 
 Widget getImage(String url,
     {double width, double height, BoxFit fit, FilterQuality filterQuality}) {
-  return Image.asset(
-    url,
-    fit: fit,
-    width: width,
-    height: height,
-    filterQuality: filterQuality,
-  );
+  // return Image.asset(
+  //   url,
+  //   fit: fit,
+  //   width: width,
+  //   height: height,
+  //   filterQuality: filterQuality,
+  // );
 
-  // return PlatformAwareAssetImage(
-  //     url: url,
-  //     fit: fit,
-  //     width: width,
-  //     height: height,
-  //     filterQuality: filterQuality);
+  return PlatformAwareAssetImage(
+      url: url,
+      fit: fit,
+      width: width,
+      height: height,
+      filterQuality: filterQuality);
 }
