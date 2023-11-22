605a9c058dacf7b5264bc20cf925a4dec5e44573
li <lilei961002@gmail.com>
2023-11-22 17:46:18 +0700

    版本升级

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ void main() async {
             '_' +
             DateTime.now().millisecondsSinceEpoch.toString().toString(),
     "bundleId": "com.pwa.pilipili",
-    "version": "2.1.2",
+    "version": "3.0.0",
     "oauth_type": CommonUtils.isAndroidWeb() ? PPString.aWeb : PPString.web,
     "language": 'zh',
     "via": 'pwa',
@@ -99,7 +99,7 @@ void main() async {
       AppGlobal.appinfo = {
         "oauth_id": iosInfo.identifierForVendor,
         "bundleId": packageInfo.packageName,
-        "version": "2.1.2",
+        "version": "3.0.0",
         "oauth_type": "ios",
       };
     }
