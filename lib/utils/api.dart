69c8f296d05e6142e1a5a027313c2a084df0fa89
li <lilei961002@gmail.com>
2023-11-22 17:42:52 +0700

    save

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -36,6 +36,7 @@ import 'http.dart';
 Future<HomeData> getHomeConfig(BuildContext context) async {
   try {
     Response<dynamic> res = await PlatformAwareHttp.post('/api/home/config');
+    CommonUtils.debugPrint(res);
     Response<dynamic> res2 =
         await PlatformAwareHttp.post('/api/privilege/getUserPrivilege');
     if (res.data['data']['help'] != null) {
