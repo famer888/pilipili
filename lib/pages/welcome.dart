e7c417bff3bbaf0ecee8b2e8a2247dbfc3a6ae14
李雷 <lilei961112@gmail.com>
2025-12-24 13:40:42 +0700

    save

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -39,13 +39,15 @@ class _WelcomeState extends State<Welcome> {
       if (traceID != null) AppGlobal.appBox?.put('trace_id', traceID);
     } else {
       Clipboard.getData(Clipboard.kTextPlain).then((value) {
-        if (value?.text != null) {
-          final params = Uri.splitQueryString(value?.text ?? '');
-          final aff = params['sq_aff'];
-          final traceID = params['trace_id'];
-          if (aff != null) toInvitation(affCode: aff);
-          if (traceID != null) AppGlobal.appBox?.put('trace_id', traceID);
-        }
+        try {
+          if (value?.text != null) {
+            final params = Uri.splitQueryString(value?.text ?? '');
+            final aff = params['sq_aff'];
+            final traceID = params['trace_id'];
+            if (aff != null) toInvitation(affCode: aff);
+            if (traceID != null) AppGlobal.appBox?.put('trace_id', traceID);
+          }
+        } catch (e) {}
       });
     }
   }
